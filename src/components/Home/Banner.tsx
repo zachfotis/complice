383d82f87b900d6f02f170691450ef18c4d48661
Fotios N. Zachopoulos <zaxfotis@gmail.com>
2023-07-21 00:30:04 +0300

    fix css

--- src/components/Home/Banner.tsx
+++ src/components/Home/Banner.tsx
@@ -2,12 +2,12 @@ import Button from '../common/Button';
 
 function Banner() {
   return (
-    <section className="my-10 flex w-full flex-col items-center justify-start gap-5 px-10 py-20 text-white bg-primary">
-      {/* Banner to get 10% with registration */}
+    <section className="my-5 md:my-10 flex w-full flex-col items-center justify-start gap-5 px-10 py-20 text-white bg-primary">
+      {/* Banner to get 10% with registration */ }
       <div className="flex w-full flex-col items-center justify-start gap-5">
         <h1 className="text-center text-h2 font-custom"> New Here? </h1>
         <h3 className="text-center text-h3 font-custom">Get 10% off your first order!</h3>
-        <Button text="Register Now" variant="sm-light"/>
+        <Button text="Register Now" variant="sm-light" />
       </div>
     </section>
   );
