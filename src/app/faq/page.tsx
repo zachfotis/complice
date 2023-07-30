b8ce70a850c007278b58cf51ee3f6067b5f066b5
Fotios N. Zachopoulos <zaxfotis@gmail.com>
2023-07-30 12:25:25 +0300

    add accordion in faq

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -3,9 +3,18 @@ import PageTemplate from '@/components/layout/PageTemplate';
 import PageBody from '@/components/layout/PageBody';
 import Categories from '@/components/layout/Categories';
 import PageTitle from '@/components/layout/PageTitle';
-import { BsBox, BsBoxArrowLeft, BsCreditCard2Back, BsGear, BsTruck } from 'react-icons/bs';
+import { BsBox, BsBoxArrowLeft, BsChevronDown, BsCreditCard2Back, BsGear, BsTruck } from 'react-icons/bs';
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+
+type Expandable = {
+  category: string;
+  index: number;
+}
 
 function Page() {
+  const [expanded, setExpanded] = useState<Expandable[]>([]);
+
   return (
     <PageTemplate>
       <PageBody>
@@ -20,10 +29,30 @@ function Page() {
                 <h1 className="text-h3 font-custom">{ category.category }</h1>
               </div>
               <div className="w-full flex flex-col items-start justify-start gap-5">
-                { category.questions.map((question, index) => (
-                  <div key={ index } className="w-full flex flex-col items-start justify-start gap-5">
-                    <h1 className="text-base font-medium">{ question.question }</h1>
-                    <p className="text-base font-light">{ question.answer }</p>
+                { category.questions.map((question, index2) => (
+                  <div key={ index2 } className="w-full flex flex-col items-start justify-start gap-5">
+                    <div className="w-full flex justify-between items-center bg-[#FBFBFB] p-5 cursor-pointer shadow-sm rounded-sm"
+                      onClick={ () => {
+                        if (expanded.some((item) => item.category === category.category && item.index === index2)) {
+                          setExpanded(expanded.filter((item) => item.category !== category.category || item.index !== index2));
+                        } else {
+                          const newExpanded = expanded.filter((item) => item.category !== category.category);
+                          setExpanded([...newExpanded, { category: category.category, index: index2 }]);
+                        }
+                      } }
+
+                    >
+                      <h1 className="text-base font-medium">{ question.question }</h1>
+                      <BsChevronDown className="text-2xl" />
+                    </div>
+                    { expanded.some((item) => item.category === category.category && item.index === index2) && (
+                      <motion.p
+                        initial={ { height: 0 } }
+                        animate={ { height: 'auto' } }
+                        transition={ { duration: 0.3 } }
+                        exit={ { height: 0 } }
+                        className="text-base font-light px-5 overflow-hidden">{ question.answer }</motion.p>
+                    ) }
                   </div>
                 )) }
               </div>
