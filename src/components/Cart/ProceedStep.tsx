bc2d014fcd9105dce2be059c49733522a0194231
Fotios N. Zachopoulos <zaxfotis@gmail.com>
2023-11-11 20:27:01 +0200

    Add checkout

--- src/components/Cart/ProceedStep.tsx
+++ src/components/Cart/ProceedStep.tsx
@@ -15,9 +15,9 @@ function ProceedStep({ currentStep, setCurrentStep, cartProducts, shippingAddres
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
-      behavior: 'smooth'
+      behavior: 'smooth',
     });
-  }
+  };
 
   useEffect(() => {
     if (currentStep === 1) {
@@ -30,34 +30,60 @@ function ProceedStep({ currentStep, setCurrentStep, cartProducts, shippingAddres
     scrollToTop();
   }, [currentStep]);
 
+  const handleCheckout = async () => {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/checkout/create-checkout-session`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify({
+        orderProducts: cartProducts,
+        shippingAddress,
+      }),
+    });
+    const data = await response.json();
+
+    if (data.url) {
+      window.open(data.url, '_self');
+    }
+  };
+
   const validateShippingAddress = () => {
-    return !(shippingAddress.firstName === '' || shippingAddress.lastName === '' || shippingAddress.address === '' || shippingAddress.city === '' || shippingAddress.postalCode === '' || shippingAddress.country === '' || shippingAddress.phoneNumber === '' || shippingAddress.email === '');
-  }
+    return !(
+      shippingAddress.firstName === '' ||
+      shippingAddress.lastName === '' ||
+      shippingAddress.address === '' ||
+      shippingAddress.city === '' ||
+      shippingAddress.postalCode === '' ||
+      shippingAddress.country === '' ||
+      shippingAddress.phoneNumber === '' ||
+      shippingAddress.email === ''
+    );
+  };
 
   return (
     <button
-      className="flex h-12 w-full max-w-[1000px] items-center justify-center border border-black bg-black text-white mt-3"
-      onClick={
-        () => {
-          if (currentStep === 1) {
-            if (cartProducts.length > 0) {
-              setCurrentStep(currentStep + 1);
-            } else {
-              toast.error('Please add some products to the cart');
-            }
-          } else if (currentStep === 2) {
-            if (validateShippingAddress()) {
-              setCurrentStep(currentStep + 1);
-            } else {
-              toast.error('Please fill in all the Shipping Details');
-            }
-          } else if (currentStep === 3) {
-            toast.success('Order placed successfully');
+      className="w-full h-12 max-w-[1000px] flex items-center justify-center border border-black bg-black text-white mt-3"
+      onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
+        if (currentStep === 1) {
+          if (cartProducts.length > 0) {
+            setCurrentStep(currentStep + 1);
+          } else {
+            toast.error('Please add some products to the cart');
           }
+        } else if (currentStep === 2) {
+          if (validateShippingAddress()) {
+            setCurrentStep(currentStep + 1);
+          } else {
+            toast.error('Please fill in all the Shipping Details');
+          }
+        } else if (currentStep === 3) {
+          handleCheckout();
         }
-      }
+      }}
     >
-      <p className="text-base font-[500]">{ buttonText }</p>
+      <p className="text-base font-[500]">{buttonText}</p>
     </button>
   );
 }
