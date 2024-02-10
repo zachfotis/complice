df175dcafc13714a82ba282dac5df0a4770b2536
Fotios N. Zachopoulos <zaxfotis@gmail.com>
2024-02-10 22:43:48 +0200

    add banner in home

--- src/components/Home/Banner.tsx
+++ src/components/Home/Banner.tsx
@@ -1,16 +1,21 @@
 import Button from '../common/Button';
 import Link from 'next/link';
+import Image from 'next/image';
+import RankImage from '@/assets/rank_banner.png';
 
 function Banner() {
   return (
-    <section className="my-0 md:my-10 flex w-full flex-col items-center justify-start gap-5 text-white mx-auto md:px-5 md:max-w-[1680px]">
-      {/* Banner to get 10% with registration */ }
-      <div className="flex w-full flex-col items-center justify-start gap-5 bg-primary px-10 py-20 ">
-        <h1 className="text-center text-h2 font-custom"> New Here? </h1>
-        <h3 className="text-center text-h3 font-custom">Get 10% off your first order!</h3>
-        <Link href={ '/auth/register' }>
-        <Button text="Register Now" variant="sm-light" />
-        </Link>
+    <section className="md:mb-10 flex w-full flex-col items-center justify-start gap-5 text-white mx-auto">
+      <div className="relative flex w-full h-[600px] flex-col items-center justify-start gap-5 p-10 md:p-20">
+        <div className="h-full flex flex-col items-center justify-center text-whiteGrey gap-5">
+          <h1 className="text-center text-h2 font-custom">New Here?</h1>
+          <h3 className="text-center text-h3 font-custom">Register and get 10% off your first order!</h3>
+          <Link href="/auth/register" className="my-5">
+            <Button text="Register Now" variant="sm-light" />
+          </Link>
+          <h3 className="text-center text-h4 font-custom">Get even more discounts and gifts by ranking up!</h3>
+        </div>
+        <Image src={ RankImage } alt="Rank Image" width={ 1600 } height={ 600 } className="w-full h-full object-cover absolute top-0 left-0 -z-10" />
       </div>
     </section>
   );
