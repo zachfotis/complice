e7bb6df39de8445a2dc55c640bebf857f30c398b
Fotios N. Zachopoulos <zaxfotis@gmail.com>
2024-02-10 20:14:03 +0200

    Add speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,24 @@
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 import Footer from '@/components/layout/Footer';
 import Navbar from '@/components/layout/Navbar';
 import ToastCustomContainer from '@/components/layout/ToastCustomContainer';
 import '@/styles/globals.css';
 import Cookies from '@/components/layout/Cookies';
+import { ReactNode } from 'react';
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className="flex min-h-screen w-full flex-col items-start justify-start bg-white text-primary">
         <Navbar />
         {children}
-        <Analytics />
         <Footer />
         <Cookies />
         <ToastCustomContainer />
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
