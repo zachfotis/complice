c7764256d079e2d1426d7ecb7f117d7872ad8263
Fotios N. Zachopoulos <zaxfotis@gmail.com>
2024-02-11 22:41:22 +0200

    Add receipt support

--- src/components/Account/SingleOrder.tsx
+++ src/components/Account/SingleOrder.tsx
@@ -5,12 +5,37 @@ import { FaShippingFast } from 'react-icons/fa';
 import { GrCompliance } from 'react-icons/gr';
 import { MdPendingActions } from 'react-icons/md';
 import { OrderType } from '../../../typings';
+import { TfiReceipt } from 'react-icons/tfi';
+import { toast } from 'react-toastify';
 
 interface Props {
   order: OrderType;
 }
 
 export function SingleOrder({ order }: Props) {
+
+  const fetchCharge = async () => {
+    if (!order) return;
+
+    try {
+      const baseUrl = process.env.NEXT_PUBLIC_API_URL;
+      const response = await fetch(`${ baseUrl }/orders/get-receipt/${ order.id }`, {
+        method: 'GET',
+        credentials: 'include',
+        cache: 'no-store',
+      });
+      const data = await response.json();
+
+      if (data?.receiptUrl) {
+        window.open(data.receiptUrl, '_blank');
+      } else {
+        toast.error('Receipt not found');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   let statusIcon = null;
   switch (order.deliveryStatus) {
     case 'pending':
@@ -26,7 +51,7 @@ export function SingleOrder({ order }: Props) {
       statusIcon = null;
   }
   return (
-    <div className="w-full flex flex-col justify-stretch items-start gap-3 rounded-sm shadow-md p-5">
+    <div className="relative w-full flex flex-col justify-stretch items-start gap-3 rounded-sm shadow-md p-5">
       <div
         className={ `text-base sm:text-lg font-bold flex justify-center items-center gap-3
           ${
@@ -41,44 +66,49 @@ export function SingleOrder({ order }: Props) {
         <p>{ order.deliveryStatus.charAt(0).toUpperCase() + order.deliveryStatus.slice(1) }</p>
         { statusIcon }
       </div>
-      { order.trackingNumber && (
-        <Link
-          href={ `https://www.taxydromiki.com/en/track/${ order.trackingNumber }` }
-          passHref
-          className="text-sm sm:text-base font-normal"
-        >
-          <strong>Tracking Number:</strong> { order.trackingNumber }
-        </Link>
-      ) }
-      <p className="text-sm sm:text-base font-normal">
-        <strong>Order:</strong> { order.id }
-      </p>
-      <p className="text-sm sm:text-base font-normal">
-        <strong>Date:</strong> { format(new Date(order.createdAt), 'LLLL d, yyyy - HH:mm') }
-      </p>
-      <p className="text-sm sm:text-base font-normal">
-        <strong>Total:</strong> { order.totalCost }€
-      </p>
-      <div className="w-full text-sm sm:text-base font-normal flex flex-col md:flex-row justify-center items-start md:items-center md:gap-5">
-        <strong>Items</strong>
-        <div className="w-full flex justify-start items-start gap-3 flex-wrap">
-          { order.products.map(
-            (product) =>
-              <div key={ product.id + product.size + product.quantity } className="relative" title={ product.title + ' - ' + product.size }>
-                <Image
-                  src={ product.imagesURL?.image1 || '' }
-                  alt={ product.title }
-                  width={ 80 }
-                  height={ 80 }
-                  className="object-contain w-[80px] h-[80px]"
-                />
-                { product.quantity > 1 && (
-                  <p className="absolute bottom-2 right-2 text-xs font-medium font-custom bg-primary text-white px-1 rounded-md">x{ product.quantity } </p>
-                ) }
-              </div>,
-          ) }
+      <div className="grid grid-cols-[auto_auto] gap-5 text-sm sm:text-base font-normal">
+        { order.trackingNumber && (
+          <>
+            <Link href={ `https://www.taxydromiki.com/en/track/${ order.trackingNumber }` } passHref>
+              <strong>Tracking Number:</strong>
+            </Link>
+            <p>{ order.trackingNumber }</p>
+          </>
+        ) }
+        <p><strong>Order:</strong></p>
+        <p>{ order.id }</p>
+        <p><strong>Date:</strong></p>
+        <p>{ format(new Date(order.createdAt), 'LLLL d, yyyy - HH:mm') }</p>
+        <p><strong>Total:</strong></p>
+        <p>{ order.totalCost }€</p>
+        <div className="w-full col-span-2 text-sm sm:text-base font-normal flex flex-col md:flex-row justify-center items-start md:items-center md:gap-5">
+          <strong>Items</strong>
+          <div className="w-full flex justify-start items-start gap-3 flex-wrap">
+            { order.products.map(
+              (product) => (
+                <div key={ product.id + product.size + product.quantity } className="relative" title={ product.title + ' - ' + product.size }>
+                  <Image
+                    src={ product.imagesURL?.image1 || '' }
+                    alt={ product.title }
+                    width={ 80 }
+                    height={ 80 }
+                    className="object-contain w-[80px] h-[80px]"
+                  />
+                  { product.quantity > 1 && (
+                    <p className="absolute bottom-2 right-2 text-xs font-medium font-custom bg-primary text-white px-1 rounded-md">x{ product.quantity } </p>
+                  ) }
+                </div>
+              )) }
+          </div>
         </div>
       </div>
-    </div>
-  );
+      <div
+        className="absolute bottom-5 right-5 flex justify-center items-center gap-2 cursor-pointer rounded-md p-2 hover:bg-primary hover:text-white"
+        onClick={ fetchCharge }>
+        <p><strong>Receipt</strong></p>
+        <TfiReceipt className="text-2xl" />
+        </div>
+      </div>
+  )
+    ;
 }
