9a9b6881c346d0137924ddabfd4987562d4ba61f
Fotios N. Zachopoulos <zaxfotis@gmail.com>
2024-07-21 18:49:29 +0300

    add toTitleCase in categories products

--- src/components/Home/CategoryHomeThumb.tsx
+++ src/components/Home/CategoryHomeThumb.tsx
@@ -1,3 +1,4 @@
+import commonUtils from '@/utils/commonUtils';
 import Image from 'next/image';
 import Link from 'next/link';
 import { CategoryType } from '../../../typings';
@@ -8,20 +9,22 @@ interface CategoryThumbProps {
 
 function CategoryHomeThumb({ category }: CategoryThumbProps) {
   return (
-    <Link href={ `/${ category.categoryType }/${ category.title }` }
-      className="flex flex-1 snap-center flex-col items-center justify-start gap-2 min-w-[250px]">
+    <Link
+      href={`/${category.categoryType}/${category.title}`}
+      className="flex flex-1 snap-center flex-col items-center justify-start gap-2 min-w-[250px]"
+    >
       <div className="w-full overflow-hidden h-[400px]">
         <Image
-          src={ category.imageURL }
+          src={category.imageURL}
           alt="Category"
-          width={ 300 }
-          height={ 300 }
+          width={300}
+          height={300}
           className="h-full w-full object-cover object-center transition-all duration-300 ease-in-out hover:scale-110"
         />
       </div>
       <div className="flex w-full flex-col items-center justify-start">
-        <h3 className="text-center text-h3 font-custom">{ category.title.charAt(0).toUpperCase() + category.title.slice(1) }</h3>
-        <p className="text-center text-base">{ category.description }</p>
+        <h3 className="text-center text-h3 font-custom">{commonUtils.toTitleCase(category.title)}</h3>
+        <p className="text-center text-base">{category.description}</p>
       </div>
     </Link>
   );
