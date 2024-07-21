c7e04555cc0fa7228703d2ef93ba00f4d3313eb6
Fotios N. Zachopoulos <zaxfotis@gmail.com>
2024-07-21 16:38:14 +0300

    update photos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,73 @@ import Footer from '@/components/layout/Footer';
 import Navbar from '@/components/layout/Navbar';
 import ToastCustomContainer from '@/components/layout/ToastCustomContainer';
 import '@/styles/globals.css';
+import { Metadata } from 'next';
 import { ReactNode } from 'react';
+
+export const metadata: Metadata = {
+  title: {
+    default: 'Complice Team',
+    template: '%s | Complice Team',
+  },
+  description:
+    'A group of friends and family coming together to unveil themselves through fashion. Complice is a slow fashion brand born and made in Greece. This commitment to slow fashion emphasizes quality over quantity and means that each piece is designed to be timeless, durable, and versatile, offering long-lasting value to its wearers.',
+  keywords: [
+    'complice',
+    'team',
+    'fashion',
+    'athens',
+    'clothes',
+    'shop',
+    'store',
+    'brand',
+    'clothing',
+    'accessories',
+    'streetwear',
+    't-shirts',
+    'hoodies',
+    'jackets',
+    'pants',
+    'shorts',
+    'caps',
+  ],
+  robots: {
+    index: true,
+    follow: true,
+  },
+  authors: {
+    name: 'Complice Team Athens',
+  },
+  creator: 'Fotios N. Zachopoulos',
+  openGraph: {
+    type: 'website',
+    url: 'https://www.compliceteam.com/',
+    title: 'Complice Team',
+    description:
+      'A group of friends and family coming together to unveil themselves through fashion. Complice is a slow fashion brand born and made in Greece. This commitment to slow fashion emphasizes quality over quantity and means that each piece is designed to be timeless, durable, and versatile, offering long-lasting value to its wearers.',
+    images: [
+      {
+        url: 'https://cf.bstatic.com/xdata/images/hotel/max1280x900/559754621.jpg?k=7d21b149515d31b8e2cf5aa28e83f945c21cc7744589f1fe3bc91e831f02e975&o=&hp=1',
+        width: 1024,
+        height: 684,
+        alt: 'Complice Team',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Complice Team',
+    description:
+      'A group of friends and family coming together to unveil themselves through fashion. Complice is a slow fashion brand born and made in Greece. This commitment to slow fashion emphasizes quality over quantity and means that each piece is designed to be timeless, durable, and versatile, offering long-lasting value to its wearers.',
+    images: [
+      {
+        url: 'https://cf.bstatic.com/xdata/images/hotel/max1280x900/559754621.jpg?k=7d21b149515d31b8e2cf5aa28e83f945c21cc7744589f1fe3bc91e831f02e975&o=&hp=1',
+        width: 1024,
+        height: 684,
+        alt: 'Complice Team',
+      },
+    ],
+  },
+};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -1,5 +1,3 @@
-import BannerImage1 from '@/assets/Alex.jpg';
-import BannerImage2 from '@/assets/Tasos.jpg';
 import Image from 'next/image';
 import Link from 'next/link';
 import Button from '../common/Button';
@@ -8,18 +6,11 @@ function Hero() {
   return (
     <article className="relative w-full mx-auto flex justify-center items-center bg-black md:p-1 md:max-w-[1680px] -mt-20 md:mt-0">
       <Image
-        src={BannerImage1}
+        src="https://complice.fra1.cdn.digitaloceanspaces.com/banner.png"
         alt="Banner"
         width={1680}
         height={600}
         className="w-full flex-1 object-cover object-center brightness-[50%] contrast-50 h-[600px]"
-      />
-      <Image
-        src={BannerImage2}
-        alt="Banner"
-        width={1680}
-        height={600}
-        className="w-full flex-1 object-cover brightness-[50%] contrast-50 object-center h-[600px]"
       />
       <div className="absolute w-full p-5 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transform text-center">
         <h1 className="mb-4 drop-shadow-lg text-h1 text-whiteGrey font-custom whitespace-pre-wrap">
