9452726d220fccd8e862d2ba55eb45b35d66116b
Fotios N. Zachopoulos <zaxfotis@gmail.com>
2024-07-23 19:38:32 +0300

    chore: Update Hero component to use lazy loading for the banner image

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -1,16 +1,17 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import HeroImage from '../../assets/hero.jpg';
 import Button from '../common/Button';
 
 function Hero() {
   return (
     <article className="relative w-full mx-auto flex justify-center items-center bg-black md:p-1 md:max-w-[1680px] -mt-20 md:mt-0">
       <Image
-        src="https://complice.fra1.cdn.digitaloceanspaces.com/banner.png"
+        src={HeroImage}
         alt="Banner"
-        width={1680}
-        height={600}
-        className="w-full flex-1 object-cover object-left brightness-[50%] contrast-50 h-[600px]"
+        width={1920}
+        height={1080}
+        className="w-full flex-1 object-cover object-center brightness-[70%] contrast-[75%] h-[600px]"
       />
       <div className="absolute w-full p-5 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transform text-center">
         <h1 className="mb-4 drop-shadow-lg text-h1 text-whiteGrey font-custom whitespace-pre-wrap">
